25c4df278404f8358a698341cea6efe14e9e5372
Pieter Soudan <pieter.soudan@redandivory.be>
2016-01-14 17:00:22 +0100

    bump version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'sewdn:locksmith',
-  version: '0.0.2',
+  version: '0.0.3',
   summary: 'Lock a function over several app instances or builds',
   git: "https://github.com/Sewdn/meteor-locksmith.git"
 });
